aab7621e72e97cb0289259e65c7debced2eac5d1
Craig Smitham <craigsmitham@gmail.com>
2014-04-02 08:22:16 -0500

    Moved chapter 1 code into module

--- Source/ProgrammingFSharp.Cli/Program.fs
+++ Source/ProgrammingFSharp.Cli/Program.fs
@@ -1,19 +1,10 @@
-﻿open System
+﻿//open System
 
 [<EntryPoint>]
 let main args = 
+    Chapter1.Cli args
     
-    if args.Length <> 2 then
-        failwith "Error: Expected arguments <greeting> and <thing>"
 
-    let greeting, thing = args.[0], args.[1]
-    let timeOfDay = DateTime.Now.ToString("hh:mm tt")
-
-    printfn "%s, %s at %s" greeting thing timeOfDay
-
-    (*
-        Example F# block comment
-    *)
 
     // return an integer exit code
     0 
